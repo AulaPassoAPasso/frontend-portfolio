de666c766151bfa520eae3242404f57c95fc047e
AulaPassoAPasso <gopsenac@gmail.com>
2024-01-17 15:47:01 -0300

    fix layout

--- src/components/Contato/Contato.jsx
+++ src/components/Contato/Contato.jsx
@@ -22,46 +22,50 @@ export const Contato = ({categoria}) => {
 
   return (
     <div id={categoria}>
-        <h2>Você pode me enviar uma mensagem! 👇</h2>
-        <p>Preencha o seu nome, seu e-mail e escreva a mensagem que deseja enviar para mim. Logo quando eu puder, irei te responder.
-          <br/><br/>Desde já, agradeço o seu contato!
-        </p>
-        <form onSubmit={handleSubmit}>
         <div>
-            <label htmlFor="nome">Nome:</label>
-            <input
-            type="text"
-            id="nome"
-            name="nome"
-            value={nome}
-            onChange={handleSubmit}
-            required
-            />
+          <h2>Você pode me enviar uma mensagem! 👇</h2>
+          <p>Preencha o seu nome, seu e-mail e escreva a mensagem que deseja enviar para mim. Logo quando eu puder, irei te responder.
+            <br/><br/>Desde já, agradeço o seu contato!
+          </p>
         </div>
         <div>
-            <label htmlFor="email">Seu e-mail:</label>
-            <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={handleSubmit}
-            required
-            placeholder="exemplo@gmail.com"
-            />
+          <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="nome">Nome:</label>
+                <input
+                type="text"
+                id="nome"
+                name="nome"
+                value={nome}
+                onChange={handleSubmit}
+                required
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Seu e-mail:</label>
+                <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={handleSubmit}
+                required
+                placeholder="exemplo@gmail.com"
+                />
+            </div>
+            <div>
+                <label htmlFor="mensagem">Mensagem:</label>
+                <textarea
+                id="mensagem"
+                name="mensagem"
+                value={mensagem}
+                onChange={handleSubmit}
+                required
+                />
+            </div>
+            <button type="submit">Enviar</button>
+          </form>
         </div>
-        <div>
-            <label htmlFor="mensagem">Mensagem:</label>
-            <textarea
-            id="mensagem"
-            name="mensagem"
-            value={mensagem}
-            onChange={handleSubmit}
-            required
-            />
-        </div>
-        <button type="submit">Enviar</button>
-        </form>
     </div>
   );
 }
